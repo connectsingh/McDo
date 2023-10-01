184417a69480a72f7de44bfa013508bf42f902dc
Chaman Deep Singh <Csingh5505@conestogac.on.ca>
2023-10-01 12:30:05 -0400

    Update assignment1Shwarma.js

--- assignment1Shwarma.js
+++ assignment1Shwarma.js
@@ -1,54 +1,60 @@
 const Order = require("./assignment1Order");
 
 const OrderState = Object.freeze({
-    WELCOMING:   Symbol("welcoming"),
-    SIZE:   Symbol("size"),
-    TOPPINGS:   Symbol("toppings"),
-    DRINKS:  Symbol("drinks")
+    WELCOMING: Symbol("welcoming"),
+    ITEM_SELECTION: Symbol("item_selection"),
+    TOPPINGS: Symbol("toppings"),
+    DRINKS: Symbol("drinks")
 });
 
-module.exports = class ShwarmaOrder extends Order{
-    constructor(){
+module.exports = class ShwarmaOrder extends Order {
+    constructor() {
         super();
         this.stateCur = OrderState.WELCOMING;
-        this.sSize = "";
+        this.selectedItem = "";
         this.sToppings = "";
         this.sDrinks = "";
-        this.sItem = "shawarama";
+        this.availableItems = ["wrap", "fries"];
     }
-    handleInput(sInput){
+
+    handleInput(sInput) {
         let aReturn = [];
-        switch(this.stateCur){
+        switch (this.stateCur) {
             case OrderState.WELCOMING:
-                this.stateCur = OrderState.SIZE;
-                aReturn.push("Welcome to Richard's Shawarma.");
-                aReturn.push("What size would you like?");
+                this.stateCur = OrderState.ITEM_SELECTION;
+                aReturn.push("Welcome to McDo.");
+                aReturn.push("What would you like to order? (wrap or fries)");
                 break;
-            case OrderState.SIZE:
-                this.stateCur = OrderState.TOPPINGS
-                this.sSize = sInput;
-                aReturn.push("What toppings would you like?");
+            case OrderState.ITEM_SELECTION:
+                if (this.availableItems.includes(sInput.toLowerCase())) {
+                    this.stateCur = OrderState.TOPPINGS;
+                    this.selectedItem = sInput.toLowerCase();
+                    aReturn.push(`You selected ${this.selectedItem}. What toppings would you like?`);
+                } else {
+                    aReturn.push("Invalid item selection. Please choose wrap or fries.");
+                }
                 break;
             case OrderState.TOPPINGS:
-                this.stateCur = OrderState.DRINKS
+                this.stateCur = OrderState.DRINKS;
                 this.sToppings = sInput;
-                aReturn.push("Would you like drinks with that?");
+                aReturn.push("Would you like a drink with that? (e.g., coffee)");
                 break;
             case OrderState.DRINKS:
                 this.isDone(true);
-                if(sInput.toLowerCase() != "no"){
+                if (sInput.toLowerCase() !== "no") {
                     this.sDrinks = sInput;
                 }
-                aReturn.push("Thank-you for your order of");
-                aReturn.push(`${this.sSize} ${this.sItem} with ${this.sToppings}`);
-                if(this.sDrinks){
-                    aReturn.push(this.sDrinks);
+                aReturn.push("Thank you for your order:");
+                aReturn.push(`- Item: ${this.selectedItem}`);
+                aReturn.push(`- Toppings: ${this.sToppings}`);
+                if (this.sDrinks) {
+                    aReturn.push(`- Drink: ${this.sDrinks}`);
                 }
-                let d = new Date(); 
+                let d = new Date();
                 d.setMinutes(d.getMinutes() + 20);
                 aReturn.push(`Please pick it up at ${d.toTimeString()}`);
                 break;
         }
         return aReturn;
     }
-}
+};
